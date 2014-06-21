96925535d8a01d276cec5ee92cc074dd2ee8603a
Richard Layman <rlayman2000@yahoo.com>
2014-06-21 05:11:57 -0400

    switch the viewport so the opengl shader would take files instead of char strings

--- src/lib/viewport.cpp
+++ src/lib/viewport.cpp
@@ -30,31 +30,10 @@ void Viewport::initialize()
     glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
 
     QOpenGLShader *vshader1 = new QOpenGLShader(QOpenGLShader::Vertex, &program1);
-    const char *vsrc1 =
-        "attribute highp vec4 vertex;\n"
-        "attribute mediump vec3 normal;\n"
-        "uniform mediump mat4 matrix;\n"
-        "varying mediump vec4 color;\n"
-        "void main(void)\n"
-        "{\n"
-        " vec3 toLight = normalize(vec3(0.0, 0.3, 1.0));\n"
-        " float angle = max(dot(normal, toLight), 0.0);\n"
-        " vec3 col = vec3(0.40, 1.0, 0.0);\n"
-        " color = vec4(col * 0.2 + col * 0.8 * angle, 1.0);\n"
-        " color = clamp(color, 0.0, 1.0);\n"
-        " gl_Position = matrix * vertex;\n"
-        "}\n";
-
-    vshader1->compileSourceCode(vsrc1);
+    vshader1->compileSourceFile("test.vert");
 
     QOpenGLShader *fshader1 = new QOpenGLShader(QOpenGLShader::Fragment, &program1);
-    const char *fsrc1 =
-        "varying mediump vec4 color;\n"
-        "void main(void)\n"
-        "{\n"
-        " gl_FragColor = color;\n"
-        "}\n";
-    fshader1->compileSourceCode(fsrc1);
+    fshader1->compileSourceFile("test.frag");
 
     program1.addShader(vshader1);
     program1.addShader(fshader1);
