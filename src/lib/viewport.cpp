0bd41a4a1404c5e717de05f6452f4129aa1c497e
Richard Layman <rlayman2000@yahoo.com>
2014-07-12 19:10:35 -0400

    reset bg color

--- src/lib/viewport.cpp
+++ src/lib/viewport.cpp
@@ -132,7 +132,7 @@ void Viewport::render(int width, int height)
 
     glDepthMask(true);
 
-    glClearColor(0.5f, 0.0f, 0.5f, 1.0f);
+    glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
