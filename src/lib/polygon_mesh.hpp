185241f3e6bc7cdaf03adac4a10d4830f544b4d5
Richard Layman <rlayman2000@yahoo.com>
2014-07-13 17:56:30 -0400

    added polygon_mesh node

---
+++ src/lib/polygon_mesh.hpp
@@ -0,0 +1,38 @@
+// =====================================================================================
+// 
+//       Filename:  polygon_mesh.hpp
+// 
+//    Description:  
+// 
+//        Version:  1.0
+//        Created:  07/13/2014 05:40:25 PM
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Richard Layman (), rlayman2000@yahoo.com
+//        Company:  
+// 
+// =====================================================================================
+#ifndef POLYGON_MESH_HPP
+#define POLYGON_MESH_HPP
+
+#include "deps.hpp"
+#include "types.hpp"
+#include "status.hpp"
+
+namespace feather
+{
+
+    namespace node
+    {
+
+        template <typename _Args> struct Node<PolygonMesh, _Args>::init(_Args args)
+        {
+            return status();
+        };
+
+    } // namespace node
+
+} // namespace feather
+
+#endif
