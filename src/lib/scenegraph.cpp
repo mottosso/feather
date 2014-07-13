ce73bba1738d616bbc6c6046900b18e805238e9d
Richard Layman <rlayman2000@yahoo.com>
2014-07-13 07:29:33 -0400

    added scenegraph code

---
+++ src/lib/scenegraph.cpp
@@ -0,0 +1,31 @@
+// =====================================================================================
+// 
+//       Filename:  scenegraph.cpp
+// 
+//    Description:  
+// 
+//        Version:  1.0
+//        Created:  07/09/2014 05:42:02 AM
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Richard Layman (), rlayman2000@yahoo.com
+//        Company:  
+// 
+// =====================================================================================
+
+#include "scenegraph.hpp"
+
+using namespace feather;
+
+status scenegraph::draw_gl() {
+
+    for(sgNode &n : scenegraph::sg)
+    {
+        std::cout << "draw node" << std::endl;
+        //node::Node<get_type<20,0>::exec(n),int>::draw_gl();
+    }
+
+    return status();
+}
+
